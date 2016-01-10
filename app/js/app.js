67106d83630e2bc8314fdef0f938dadd262a1d2a
dima716 <Dima716@yandex.ru>
2016-01-10 21:28:48 +0300

    Code guide v1

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,6 @@
 import 'prismjs';
+import 'prismjs/components/prism-stylus.js';
+import 'prismjs/components/prism-jade.js';
+import 'prismjs/components/prism-javascript.js';
+import 'prismjs/components/prism-git.js';
+
